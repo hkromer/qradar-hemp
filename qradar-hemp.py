d3a272b06c2126b15464780d9b5a6c6e455bbae5
Hubert Kromer <hubert.kromer@me.com>
2023-12-07 19:44:38 +0100

    Update qradar-hemp.py
    Removed old email address from the code

--- qradar-hemp.py
+++ qradar-hemp.py
@@ -3,7 +3,7 @@
 #
 #  qradar-hemp.py
 #  
-#  Copyright 2017 Hubert Kromer <hubert.kromer@de.ibm.com>
+#  Copyright 2017 Hubert Kromer
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
